46171ee866bfe440a82c27b6390b390ffed38661
Maharaaniii <umimaharani08@gmail.com>
2026-09-19 12:53:41 +0700

    rapikan navbar dan tambah halaman riwayat transaksi

---
+++ resources/views/transactions/index.blade.php
@@ -0,0 +1,12 @@
+<div>
+    @extends('layouts.app')
+    @section('title', 'Riwayat Transaksi')
+
+@section('content')
+<h1 class="text-lg font-semibold mb-4">Riwayat Transaksi</h1>
+
+<div class="border rounded-md p-4 bg-white">
+    <p class="text-slate-500">Daftar transaksi akan ditampilkan di sini.</p>
+</div>
+@endsection
+</div>
